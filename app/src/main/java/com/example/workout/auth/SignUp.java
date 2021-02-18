3ba2ce6a033709591991967fb6a6c1edadeb706c
Promisas <Promiseup4@gmail.com>
2021-02-18 23:47:04 +0200

    login & SignUp layout

--- app/src/main/java/com/example/workout/auth/SignUp.java
+++ app/src/main/java/com/example/workout/auth/SignUp.java
@@ -1,15 +1,16 @@
-package com.example.workout;
+package com.example.workout.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+import com.example.workout.R;
+
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
